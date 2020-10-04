1e7d27e0a264d4087bc657120ea119c5558e03d7
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 15:52:34 +0100

    Corrected deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/gitlabtoken/$GITLAB_TOKEN/g" docker-compose.do.yml
 scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.* $douser@$doip:~
-ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker-compose up -f docker-compose.yml -f docker-compose.do.yml'
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker-compose -f docker-compose.yml -f docker-compose.do.yml up'
