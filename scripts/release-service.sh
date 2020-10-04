861a6e556698e18d37f11fbaaa00b8b3bf4a7bc8
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 21:41:41 +0100

    Made release script compatible with posix sed

--- scripts/release-service.sh
+++ scripts/release-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 image=$1
 tag=$2
-sed -i '' "s/image\: andykuszyk\/$image\:.*/image: andykuszyk\/$image:$tag/g" docker-compose.yml
+sed -i "s/image\: andykuszyk\/$image\:.*/image: andykuszyk\/$image:$tag/g" docker-compose.yml
 git status
 git add docker-compose.yml
 git commit -m "Auto-release of $image to andykuszyk/$image:$tag"
