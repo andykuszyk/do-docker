8ab6a2c56667f6abd5de00fd3315260238a036bb
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 16:01:55 +0100

    Added nginx config to deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i "s/gitlabtoken/$GITLAB_TOKEN/g" docker-compose.do.yml
 scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.* $douser@$doip:~
+scp -i id_rsa -o "StrictHostKeyChecking no" ./nginx.conf $douser@$doip:~
 ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker-compose -f docker-compose.yml -f docker-compose.do.yml up'
