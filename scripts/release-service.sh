5d30882bb53498c8cd97d8f551c54aff1b04976a
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 21:27:59 +0100

    Added git operations to script

--- scripts/release-service.sh
+++ scripts/release-service.sh
@@ -2,3 +2,7 @@
 image=$1
 tag=$2
 sed -i '' "s/image\: andykuszyk\/$image\:.*/image: andykuszyk\/$image:$tag/g" docker-compose.yml
+git status
+git add docker-compose.yml
+git commit -m "Auto-release of $image to andykuszyk/$image:$tag"
+git push
