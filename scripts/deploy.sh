de8f895ed4a8e1b27df64909b0fbd94a92451848
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 15:36:33 +0100

    Encrypted gitlab token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "s/gitlabtoken/$gitlabToken/g" docker-compose.do.yml
+sed -i "s/gitlabtoken/$GITLAB_TOKEN/g" docker-compose.do.yml
 scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.* $douser@$doip:~
 ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker-compose up -f docker-compose.yml -f docker-compose.do.yml'
