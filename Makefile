6b9cd9531ff9bd0e471d74332891747cec8dd4da
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-05 10:24:42 +0100

    Added manual merging of docker-compose files

--- Makefile
+++ Makefile
@@ -15,3 +15,8 @@ deploy:
 
 release-service:
 	./scripts/release-service.sh $(SERVICE) $(TAG)
+
+install-yq:
+	wget https://github.com/mikefarah/yq/releases/download/3.4.0/yq_linux_amd64
+	chmod +x yq_linux_amd64
+	sudo mv yq_linux_amd64 /usr/bin/yq
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sed -i "s/gitlabtoken/$GITLAB_TOKEN/g" docker-compose.do.yml
-scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.* $douser@$doip:~
+yq m -x docker-compose.yml docker-compose.do.yml > docker-compose.release.yml
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.release.yml $douser@$doip:~
 scp -i id_rsa -o "StrictHostKeyChecking no" ./nginx.conf $douser@$doip:~
-ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker stack deploy -c docker-compose.yml -c docker-compose.do.yml szyk'
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker stack deploy -c docker-compose.release.yml szyk'
