4de26fcd4d546e801fe3ed668c72d5f2a6ca573f
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2021-06-22 12:55:12 +0100

    Moved release pipeline into github actions, but temporarily disabled deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sed -i "s/gitlabtoken/$GITLAB_TOKEN/g" docker-compose.do.yml
-sed -i "s/opencart_username/$opencart_username/g" docker-compose.do.yml
-sed -i "s/opencart_password/$opencart_password/g" docker-compose.do.yml
-sed -i "s/opencart_email/$opencart_email/g" docker-compose.do.yml
-sed -i "s/opencart_database_user/$opencart_database_user/g" docker-compose.do.yml
-sed -i "s/opencart_database_password/$opencart_database_password/g" docker-compose.do.yml
-sed -i "s/mariadb_root_password/$mariadb_root_password/g" docker-compose.do.yml
+sed -i "s/opencart_username/$OPENCART_USERNAME/g" docker-compose.do.yml
+sed -i "s/opencart_password/$OPENCART_PASSWORD/g" docker-compose.do.yml
+sed -i "s/opencart_email/$OPENCART_EMAIL/g" docker-compose.do.yml
+sed -i "s/opencart_database_user/$OPENCART_DATABASE_USER/g" docker-compose.do.yml
+sed -i "s/opencart_database_password/$OPENCART_DATABASE_PASSWORD/g" docker-compose.do.yml
+sed -i "s/mariadb_root_password/$MARIADB_ROOT_PASSWORD/g" docker-compose.do.yml
 yq m -x docker-compose.yml docker-compose.do.yml > docker-compose.release.yml
-scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.release.yml $douser@$doip:~
-scp -i id_rsa -o "StrictHostKeyChecking no" ./nginx.conf $douser@$doip:~
-ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip 'docker stack deploy -c docker-compose.release.yml szyk'
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.release.yml $DOUSER@$DOIP:~
+#scp -i id_rsa -o "StrictHostKeyChecking no" ./nginx.conf $DOUSER@$DOIP:~
+#ssh -i id_rsa -o "StrictHostKeyChecking no" $DOUSER@$DOIP 'docker stack deploy -c docker-compose.release.yml szyk'
