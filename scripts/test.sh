25ee543f81bd053d32aee6acbf4069004a0ff703
andykuszyk <andy@kuszyk.com>
2023-01-25 21:17:51 +0000

    Removed akuszyk.com from tests
    its not working at the moment and is blocking deployments

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,8 +12,6 @@ function test() {
 
 test http://budjot.com
 test https://budjot.com
-test http://akuszyk.com
-test https://akuszyk.com
 test http://marmyandme.co.uk
 test https://marmyandme.co.uk
 test http://shop.marmyandme.co.uk
